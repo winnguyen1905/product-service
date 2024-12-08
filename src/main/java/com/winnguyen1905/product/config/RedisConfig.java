2b33cec2a09344871b321e91ea33136fbbb2b782
Win Nguyen <winnguyen1905.work@gmail.com>
2024-12-08 18:58:33 +0700

    config redis for catching user permission in 60 second

--- src/main/java/com/winnguyen1905/product/config/RedisConfig.java
+++ src/main/java/com/winnguyen1905/product/config/RedisConfig.java
@@ -1,9 +1,17 @@
 package com.winnguyen1905.product.config;
 
+import java.time.Duration;
+
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 class RedisConfig {
@@ -19,5 +27,15 @@ class RedisConfig {
     RedisTemplate<String, Object> template = new RedisTemplate<>();
     template.setConnectionFactory(jedisConnectionFactory());
     return template;
+  }
+
+  @Bean
+  public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+    RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+        .entryTtl(Duration.ofSeconds(60))
+        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json()))
+        .disableCachingNullValues();
+    return RedisCacheManager.builder(redisConnectionFactory)
+        .cacheDefaults(cacheConfig).build();
   }
 }
